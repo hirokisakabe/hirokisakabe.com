b29901dea5848e915341375558dd6454c47e3cef
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2023-11-07 10:04:25 +0000

    記事詳細ページに記事の投稿日を表示する

--- src/components/post-detail.tsx
+++ src/components/post-detail.tsx
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 import { fetchPostDetail } from "@/lib/post";
 
 export async function PostDetail({ id }: { id: string }) {
@@ -17,6 +18,9 @@ export async function PostDetail({ id }: { id: string }) {
         <div className="p-3">
           <div className="pb-3">
             <div className="text-2xl">{postDetail.title}</div>
+            <div className="py-1 text-sm text-slate-600">
+              {format(new Date(postDetail.publishedAt), "yyyy/MM/dd")}
+            </div>
           </div>
           <div
             className="prose prose-sm max-w-none prose-h1:text-xl prose-h2:text-lg prose-h3:text-base prose-img:max-w-xl prose-img:rounded-xl"
